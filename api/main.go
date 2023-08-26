9865e65b81f772ae30de47c25a86dfe371b82f76
Dhiya Rifqi <dhiya@idtrust.id>
2023-08-26 17:19:29 +0700

    update

--- api/main.go
+++ api/main.go
@@ -7,21 +7,45 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	r := gin.Default()
-	r.Use(cors.Default())
+func VercelHandler(w http.ResponseWriter, r *http.Request) {
+	// Create a Gin router
+	router := gin.New()
+	router.Use(cors.Default())
+	router.Use(gin.Recovery())
 
-	r.GET("/api/hello", func(c *gin.Context) {
+	router.GET("/api/hello", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, World!")
 	})
 
-	r.POST("/api/post", func(c *gin.Context) {
+	router.POST("/api/post", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, post!")
 	})
 
-	r.GET("/api", func(c *gin.Context) {
+	router.GET("/api", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, Little mangamee!")
 	})
 
-	r.Run()
+	// Define your routes here using router.GET(), router.POST(), etc.
+
+	// Use Gin's ServeHTTP method to handle the request
+	router.ServeHTTP(w, r)
 }
+
+// func main() {
+// 	r := gin.Default()
+// 	r.Use(cors.Default())
+
+// 	r.GET("/api/hello", func(c *gin.Context) {
+// 		c.String(http.StatusOK, "Hello, World!")
+// 	})
+
+// 	r.POST("/api/post", func(c *gin.Context) {
+// 		c.String(http.StatusOK, "Hello, post!")
+// 	})
+
+// 	r.GET("/api", func(c *gin.Context) {
+// 		c.String(http.StatusOK, "Hello, Little mangamee!")
+// 	})
+
+// 	r.Run()
+// }
